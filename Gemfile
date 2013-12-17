abd91f2578400ef88abf1dbef4c861ccb85c076a
Giacomo Patella <giacomopatella@gmail.com>
2013-12-17 16:25:25 +0000

    Added user accounts / Flash message if re-type pwd doesn't match

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'database_cleaner'
 gem 'capybara'
+gem 'bcrypt-ruby'
+gem 'rack-flash3'
 
 group :development, :test do
 
--- server.rb
+++ server.rb
@@ -1,22 +1,36 @@
 require 'sinatra'
 require 'data_mapper'
+require 'rack-flash'
 
 env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
+require './lib/user'
+require './lib/tag'
+
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
+enable :sessions
+ use Rack::Flash
+set :session_secret, 'super secret'
+
+
 get '/' do 
   @links = Link.all  
-  erb:index
+  erb :index
 end
 
 get '/tags/:text' do
   tag = Tag.first(:text => params[:text])
   @links = tag ? tag.links : []
   erb :index
+end
+
+get '/users/new' do
+  @user = User.new
+  erb :"users/new"
 end
 
 
@@ -28,4 +42,25 @@ post '/links' do
   end
   Link.create(:url => url, :title => title, :tags => tags)
   redirect to('/')
+end
+
+post '/users' do
+  @user = User.new(:email => params[:email],
+                  :password => params[:password],
+                  :password_confirmation => params[:password_confirmation]
+                  )
+  if @user.save
+    session[:user_id] = @user.id
+    redirect to('/')
+  else
+    flash[:notice] = "Sorry, your passwords don't match"
+    erb :"users/new"
+  end
+end
+
+
+helpers do
+  def current_user    
+    @current_user ||=User.get(session[:user_id]) if session[:user_id]
+  end
 end

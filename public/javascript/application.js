261de3652e9c78908e32f0281af65372eaf0bdb9
Giacomo Patella <giacomopatella@gmail.com>
2014-01-09 14:23:29 +0000

    storing favourited using data()

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -7,11 +7,16 @@ $(function() {
 })
 
 function addFavouritesHandler() {
-  $(".star.solid").click(function() {
-    $(this).animate({opacity: 1}, 500);
+  $(".star.solid").click(function(event) {
+    var link = $(this).parent();
+    var favourited = !!$(link).data("favourited");
+    var newOpacity = favourited ? 0 : 1;
+    $(link).data("favourited", !favourited);
+    $(this).animate({opacity: newOpacity}, 500);
   });
 }
 
 $(function() {
   addFavouritesHandler();
 })
+
